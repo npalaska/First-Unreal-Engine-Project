ac774bf9cc6e78f7d6c0dbd41fbc8d05b01a0751
npalaska <npalaska@umflint.edu>
2018-01-31 03:04:17 -0500

    accessing actor locations, creating a new room and adding movable objects

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -1,10 +1,11 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionReport.h"
+#include "OpenDoor.h"
 #include "GameFramework/Actor.h"
 
+
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UOpenDoor::UOpenDoor()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -15,21 +16,20 @@ UPositionReport::UPositionReport()
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	FString ObjectName = GetOwner()->GetName();
-	float x = GetOwner()->GetActorLocation().X;
-	float y = GetOwner()->GetActorLocation().Y;
-	float z = GetOwner()->GetActorLocation().Z;
-	UE_LOG(LogTemp, Warning, TEXT("This is a msg from %s and its located at x: %f , y: %f , z: %f"), *ObjectName, x, y, z);
-	
+
+	AActor *Owner = GetOwner();
+
+	FRotator RotationObject = FRotator(0.0f, 30.0f, 0.0f);
+
+	Owner->SetActorRotation(RotationObject);
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
